4a225cb5a1722bb80076e3819a9541e9a7303e65
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2020-12-04 16:09:30 +0000

    Day2 - part2

--- src/day2.cpp
+++ src/day2.cpp
@@ -6,17 +6,32 @@ using namespace std;
 
 vector<string> read_data(string file_path);
 
-int count_valid_passwords(vector<string> data)
+bool sled_policy(int min, int max, char letter, string password)
 {
-    int n, min, max, valid_passwords = 0;
+    int n = count(password.begin(), password.end(), letter);
+    return (n >= min && n <= max);
+}
+
+bool toboggan_policy(int pos1, int pos2, char letter, string password)
+{
+    if (password[pos1 - 1] == letter && password[pos2 - 1] == letter)
+        return false;
+    if (password[pos1 - 1] == letter || password[pos2 - 1] == letter)
+        return true;
+    return false;
+}
+
+int count_valid_passwords(vector<string> data, string policy)
+{
+    int n, pos1, pos2, valid_passwords = 0;
     char letter;
     string password;
 
     for (string str : data)
     {
-        min = stoi(str.substr(0, str.find("-")));
+        pos1 = stoi(str.substr(0, str.find("-")));
         str.erase(0, str.find("-") + 1);
-        max = stoi(str.substr(0, str.find(" ")));
+        pos2 = stoi(str.substr(0, str.find(" ")));
         str.erase(0, str.find(" ") + 1);
 
         letter = str[0];
@@ -24,7 +39,9 @@ int count_valid_passwords(vector<string> data)
 
         n = count(password.begin(), password.end(), letter);
 
-        if (n >= min && n <= max)
+        if (policy == "sled" && sled_policy(pos1, pos2, letter, password))
+            valid_passwords++;
+        else if (policy == "toboggan" && toboggan_policy(pos1, pos2, letter, password))
             valid_passwords++;
     }
 
@@ -33,8 +50,14 @@ int count_valid_passwords(vector<string> data)
 
 int main()
 {
-    int example_passwords = count_valid_passwords(read_data("data/day2/example.txt"));
-    int input_passwords = count_valid_passwords(read_data("data/day2/input.txt"));
+    int example_passwords = count_valid_passwords(read_data("data/day2/example.txt"), "sled");
+    int input_passwords = count_valid_passwords(read_data("data/day2/input.txt"), "sled");
+
+    cout << "Number of valid passwords (example) = " << example_passwords << endl;
+    cout << "Number of valid passwords (input) = " << input_passwords << endl;
+
+    example_passwords = count_valid_passwords(read_data("data/day2/example.txt"), "toboggan");
+    input_passwords = count_valid_passwords(read_data("data/day2/input.txt"), "toboggan");
 
     cout << "Number of valid passwords (example) = " << example_passwords << endl;
     cout << "Number of valid passwords (input) = " << input_passwords << endl;
